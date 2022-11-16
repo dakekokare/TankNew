4a5462de41f762f580246e4538cca47bd60797a6
KatouKentaro <kato.kentaro2023@gmail.com>
2022-11-16 15:28:18 +0900

    ランダム生成、弾共有

--- TankTest/Assets/Otsubo/Scripts/ShotShell.cs
+++ TankTest/Assets/Otsubo/Scripts/ShotShell.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ShotShell : MonoBehaviour
+using Photon.Pun;
+public class ShotShell : MonoBehaviourPunCallbacks
 {
     public float shotSpeed;
 
@@ -18,31 +18,35 @@ public class ShotShell : MonoBehaviour
 
     void Update()
     {
+
         // タイマーの時間を動かす
         timer += Time.deltaTime;
 
-        // もしもSpaceキーを押したならば（条件）
-        // 「Space」の部分を変更することで他のキーにすることができる（ポイント）
-        if (Input.GetKeyDown(KeyCode.Space) && timer > timeBetweenShot)
+        if (photonView.IsMine)
         {
-            // タイマーの時間を０に戻す。
-            timer = 0.0f;
+            // もしもSpaceキーを押したならば（条件）
+            // 「Space」の部分を変更することで他のキーにすることができる（ポイント）
+            if (Input.GetKeyDown(KeyCode.Space) && timer > timeBetweenShot)
+            {
+                // タイマーの時間を０に戻す。
+                timer = 0.0f;
 
-            // 砲弾のプレハブを実体化（インスタンス化）する。
-            GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity);
+                // 砲弾のプレハブを実体化（インスタンス化）する。
+                GameObject shell = Instantiate(shellPrefab, transform.position, Quaternion.identity);
 
-            // 砲弾に付いているRigidbodyコンポーネントにアクセスする。
-            Rigidbody shellRb = shell.GetComponent<Rigidbody>();
+                // 砲弾に付いているRigidbodyコンポーネントにアクセスする。
+                Rigidbody shellRb = shell.GetComponent<Rigidbody>();
 
-            // forward（青軸＝Z軸）の方向に力を加える。
-            shellRb.AddForce(transform.forward * shotSpeed);
+                // forward（青軸＝Z軸）の方向に力を加える。
+                shellRb.AddForce(transform.forward * shotSpeed);
 
-            // 発射した砲弾を３秒後に破壊する。
-            // （重要な考え方）不要になった砲弾はメモリー上から削除すること。
-            Destroy(shell, 3.0f);
+                // 発射した砲弾を３秒後に破壊する。
+                // （重要な考え方）不要になった砲弾はメモリー上から削除すること。
+                Destroy(shell, 3.0f);
 
-            // 砲弾の発射音を出す。
-            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+                // 砲弾の発射音を出す。
+                AudioSource.PlayClipAtPoint(shotSound, transform.position);
+            }
         }
     }
 }
