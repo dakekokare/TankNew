e5fcd8e776cd4d8f14606d8e3665682bc495b39a
KatouKentaro <kato.kentaro2023@gmail.com>
2022-12-06 12:04:05 +0900

    count starti 調整

--- TankTest/Assets/Katou/Scripts/PlayerListCount.cs
+++ TankTest/Assets/Katou/Scripts/PlayerListCount.cs
@@ -24,7 +24,7 @@ public class PlayerListCount : MonoBehaviourPunCallbacks
         {
             count++;
             //2人いたら
-            if (count==1)
+            if (count==2)
             {
                 //// カウントダウン生成する
                 Instantiate(countDown);
